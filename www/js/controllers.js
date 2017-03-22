4d1288e57950dce0683f78118478d4c0b13dba34
roacjae <jeroach@wisc.edu>
2017-03-22 17:03:44 -0500

    Added markers when clicks are made

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -16,13 +16,7 @@ module
     var mapOptions = {
       center: latLng,
       zoom: 15,
-      mapTypeId: google.maps.MapTypeId.ROADMAP,
-      events:{
-        click: function(){
-          console.log("Map Clicked");
-
-        }
-      }
+      mapTypeId: google.maps.MapTypeId.ROADMAP
     };
 
     $scope.map = new google.maps.Map(document.getElementById("map"), mapOptions);
@@ -43,8 +37,20 @@ module
 
     google.maps.event.addListener(marker,'click', function(){
       infoWindow.open($scope.map, marker);
-    })
+    }),
+
+      google.maps.event.addListener($scope.map, 'click', function(event) {
+
+
+
+        var marker = new google.maps.Marker({
+          map: $scope.map,
+          animation: google.maps.Animation.DROP,
+          position: event.latLng
+        });
+      });
     });
 
 
-});
+
+  });
