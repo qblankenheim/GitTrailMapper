52bc6dc3db6f50ac663eec4d9ad1f763cd8245ac
roacjae <jeroach@wisc.edu>
2017-03-22 16:40:37 -0500

    Added a map to the map screen. Messing around with markers

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -5,6 +5,46 @@ module
 
 })
 
-.controller('mapsCtrl', function($scope, $ionicLoading) {
+.controller('mapsCtrl', function($scope, $state) {
 
-})
+  var options = {timeout: 10000, enableHighAccuracy: true};
+
+
+
+    var latLng = new google.maps.LatLng(43.071278, -89.406797);
+
+    var mapOptions = {
+      center: latLng,
+      zoom: 15,
+      mapTypeId: google.maps.MapTypeId.ROADMAP,
+      events:{
+        click: function(){
+          console.log("Map Clicked");
+
+        }
+      }
+    };
+
+    $scope.map = new google.maps.Map(document.getElementById("map"), mapOptions);
+
+
+
+    google.maps.event.addListenerOnce($scope.map, 'idle', function(){
+      var marker = new google.maps.Marker({
+        map: $scope.map,
+        animation: google.maps.Animation.DROP,
+        position: latLng
+      });
+
+
+  var infoWindow = new google.maps.InfoWindow({
+    content: "Computer Science Building!"
+  });
+
+    google.maps.event.addListener(marker,'click', function(){
+      infoWindow.open($scope.map, marker);
+    })
+    });
+
+
+});
