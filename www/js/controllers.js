b3688b9d6b15cc0c82cec321a5f5bb0d2c666bc9
Galen Erickson <gkerickson@wisc.edu>
2017-03-23 22:29:58 -0500

    Transition Through Different Words In Home

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,26 +1,58 @@
 
 module
-.controller('homeCtrl', function($scope, $ionicLoading) {
+.controller('homeCtrl', function($scope, $ionicLoading, $timeout) {
 
-	var wordList = ['Run','Walk','Hike','Experience','Live','Explore'];
-	$scope.explore = 'another';
+	var wordList = ['Walk','Hike','Experience','Live','Explore'];
+  $scope.explore = 'Run';
+  // function define($in){
+  //   console.log("TEST")
+  //   if($in.length == 0)
+  //     return $in;
+  //   $scope.explore = $in.pop();
+  //   define($in).then(function(response){
+  //       return define($in);
+  //     },1000);
+  // }
 
-	var define = {
-		define = function($in,$define){
-			if($in.length == 0)
-				return;
-			$scope.explore = $in.pop();
-			define($in).then(function(response){
-			     if (timer === null) {
-			         timer = $interval(function(){
-			             $scope.checkNewOrders();
-			         }.bind(this), 1000);
-			     }
-			     define($in);
-			});
-		}
-	}
-	define(wordList);
+  function doSomething(input){
+    return $timeout(function(){
+      return $scope.explore = input;
+    },1000);
+  }
+
+  function run(objects) {
+      var cntr = 0;
+      console.log(objects);
+      function next() {
+          if (cntr < objects.length) {
+              doSomething(objects[cntr++]).then(next);
+          }
+      }
+      next();
+  }
+
+  run(wordList);
+
+	// var define = {
+	// 	define = function($in,$define){
+	// 		if($in.length == 0)
+	// 			return;
+	// 		$scope.explore = $in.pop();
+	// 		define($in).then(function(response){
+	// 		     if (timer === null) {
+	// 		         timer = $interval(function(){
+	// 		             $scope.checkNewOrders();
+	// 		         }.bind(this), 1000);
+	// 		     }
+	// 		     define($in);
+	// 		});
+	// 	}
+	// }
+
+
+
+
+
 })
 
 .controller('mapsCtrl', function($scope, $ionicLoading) {
